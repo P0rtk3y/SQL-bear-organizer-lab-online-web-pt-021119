51a561dfff4efa7e409678eb5793a7f1a06370c7
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-04-18 05:56:15 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,3 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+  VALUES (1, "Mr. Chocolate", 2, "male", "chocolate", "sniffy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
