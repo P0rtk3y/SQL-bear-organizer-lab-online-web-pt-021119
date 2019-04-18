7de74a6610974758c7ad346a6c2e05861d4c1372
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-04-18 05:57:30 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,5 +2,5 @@ INSERT INTO bears (id, name, age, gender, color, temperament, alive)
   VALUES (1, "Mr. Chocolate", 2, "male", "chocolate", "sniffy", 1);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive)
   VALUES (2, "Rowdy", 1, "female", "red", "boisterous", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive)
-  VALUES (3, "Tabitha", 8, "female", "yellow", ")
+INSERT INTO bears (id, name, age, gender, color, temperament, 1)
+  VALUES (3, "Tabitha", 8, "female", "yellow", "withdrawn", 0)
